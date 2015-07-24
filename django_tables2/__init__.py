e396cf67e3e211dfd428fe0e055966e96e56cd68
Samantha Zeitlin <samanthazeitlin@gmail.com>
2015-07-24 10:17:47 -0700

    changed import of new editablecolumn file

--- django_tables2/__init__.py
+++ django_tables2/__init__.py
@@ -3,13 +3,18 @@
 from .tables  import Table
 from .columns import (BooleanColumn, Column, CheckBoxColumn, DateColumn,
                       DateTimeColumn, EmailColumn, FileColumn, LinkColumn,
-                      TemplateColumn, URLColumn, TimeColumn, EditableColumn)
+                      TemplateColumn, URLColumn, TimeColumn)
 from .config  import RequestConfig
 from .utils   import A, Attrs
 try:
     from .views import SingleTableMixin, SingleTableView
 except ImportError:
     pass
+
+try:
+   from .columns.editablecolumn import EditableColumn
+except ImportError:
+   print("still cannot import your new column type EditableColumn")
 
 
 __version__ = "1.0.4"
